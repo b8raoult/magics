7846cc0e8674da7836d1eb8368c8c971b09400db
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-10-12 09:01:33 +0100

    Settings

---
+++ src/basic/MagicsGlobal.cc
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/*! \file MagicsGlobal.h
+    \brief Implementation of Global values of Magics.
+    \author Meteorological Visualisation Section, ECMWF
+
+    Started: JUme 2020
+*/
+
+
+#include "MagicsGlobal.h"
+#include "magics.h"
+
+namespace magics {
+
+
+}  // namespace magics
