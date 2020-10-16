f1d6fc4af6d4e599a78ec1be77ab504870989f2a
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-10-16 08:34:41 +0100

    Units

---
+++ src/common/Units.cc
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*! \file Units.cc
+    \brief Implementation of Units class.
+
+
+    Changes:
+
+
+*/
+#include "Units.h"
+
+
+using namespace magics;
+
+ bool Units::convert(const std::string& from, const std::string& to, double& scaling, double& offset) {
+    return false;
+}
